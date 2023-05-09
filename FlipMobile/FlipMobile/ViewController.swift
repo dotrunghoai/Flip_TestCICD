db0b309f0878de95497c9343d57f2b218c3bc7d6
Do Trung Hoai <trunghoai.do@samsung.com>
2023-05-09 10:16:49 +0700

    first commit

---
+++ FlipMobile/FlipMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlipMobile
+//
+//  Created by iOS_hoai_new on 09/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
