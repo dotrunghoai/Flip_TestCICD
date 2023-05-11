5c92276126292a6f4528835cb0cda588397f49f0
Do Trung Hoai <trunghoai.do@samsung.com>
2023-05-11 13:08:26 +0700

    Change any thing

--- Core/ScreenshareCore/ScreenshareCore/EnterURLSwiftUIViewModel.swift
+++ Core/ScreenshareCore/ScreenshareCore/EnterURLSwiftUIViewModel.swift
@@ -5,12 +5,12 @@
 //  Created by iOS_hoai_new on 09/05/2023.
 //
 
-import SocketIO
+//import SocketIO
 
 class EnterURLSwiftUIViewModel {
     
     init() {
-        let socketManager = SocketManager(socketURL: URL(string: "1")!)
+//        let socketManager = SocketManager(socketURL: URL(string: "1")!)
     }
     
     func checkUrl() -> Bool {
