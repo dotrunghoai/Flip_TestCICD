34d4352960b6e66ca42b88d4dbc092288e5f7039
Do Trung Hoai <trunghoai.do@samsung.com>
2023-05-18 10:07:52 +0700

    Change any thing

--- FlipMobile/FlipMobile/ViewController.swift
+++ FlipMobile/FlipMobile/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 //        let socketManager = SocketManager(socketURL: URL(string: "1")!)
         let animationView = LottieAnimationView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        
+        
+        let a: Int = Int("2")!
     }
 }
 
